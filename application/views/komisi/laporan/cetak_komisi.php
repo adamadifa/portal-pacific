a761c42eb3f58a75e35663a48657f485582d3792
adamadifa <adamabdi.al.a@gmail.com>
2020-12-18 00:50:17 +0700

    Pembuatan Laporan Komisi Belum Fix

--- application/views/komisi/laporan/cetak_komisi.php
+++ application/views/komisi/laporan/cetak_komisi.php
@@ -21,17 +21,57 @@ function formatnumber($nilai)
       <th rowspan="2">ID KARYAWAN</th>
       <th rowspan="2">NAMA KARYAWAN</th>
       <th colspan="3">TARGET & REALISASI KATEGORI A (BB)(50)</th>
+      <th colspan="3">TARGET & REALISASI KATEGORI B (ALL AIDA)(25)</th>
+      <th colspan="3">TARGET & REALISASI KATEGORI PF (PRODUCT FOCUS)(25)</th>
+      <th colspan="3">TARGET & REALISASI CASHIN</th>
     </tr>
     <tr>
       <th>TARGET</th>
       <th>REALISASI</th>
-      <th>PRODUCT FOCUS</th>
+      <th>REALISASI/TARGET * 50</th>
+      <th>TARGET</th>
+      <th>REALISASI</th>
+      <th>REALISASI/TARGET * 25</th>
+      <th>TARGET</th>
+      <th>REALISASI</th>
+      <th>REALISASI/TARGET * 25</th>
+      <th>TARGET</th>
+      <th>REALISASI</th>
+      <th>%</th>
     </tr>
   </thead>
   <tbody style="font-size:14px !important">
     <?php
     $no = 1;
+    $poinkategoriA = 50;
+    $poinkategoriB = 25;
+    $poinkategoriPF = 25;
+
     foreach ($komisi as $k) {
+      if (!empty($k->targetkategoriA)) {
+        $hasilkategoriA = ($k->realisasitargetA / $k->targetkategoriA) * 50;
+      } else {
+        $hasilkategoriA = 0;
+      }
+
+      if (!empty($k->targetkategoriB)) {
+        $hasilkategoriB = ($k->realisasitargetB / $k->targetkategoriB) * 25;
+      } else {
+        $hasilkategoriB = 0;
+      }
+
+      if (!empty($k->targetproductfocus)) {
+        $hasilkategoriPF = ($k->realisasitargetproductfocus / $k->targetproductfocus) * 25;
+      } else {
+        $hasilkategoriPF = 0;
+      }
+
+      if (!empty($k->jumlah_target_cashin)) {
+        $hasilcashin = ($k->jml_cashin / $k->jumlah_target_cashin) * 100;
+      } else {
+        $hasilcashin = 0;
+      }
+
     ?>
       <tr>
         <td><?php echo $no; ?></td>
@@ -39,6 +79,16 @@ function formatnumber($nilai)
         <td><?php echo $k->nama_karyawan; ?></td>
         <td align="right"><?php echo formatnumber($k->targetkategoriA); ?></td>
         <td align="right"><?php echo formatnumber($k->realisasitargetA); ?></td>
+        <td align="right"><?php echo formatnumber($hasilkategoriA); ?></td>
+        <td align="right"><?php echo formatnumber($k->targetkategoriB); ?></td>
+        <td align="right"><?php echo formatnumber($k->realisasitargetB); ?></td>
+        <td align="right"><?php echo formatnumber($hasilkategoriB); ?></td>
+        <td align="right"><?php echo formatnumber($k->targetproductfocus); ?></td>
+        <td align="right"><?php echo formatnumber($k->realisasitargetproductfocus); ?></td>
+        <td align="right"><?php echo formatnumber($hasilkategoriPF); ?></td>
+        <td align="right"><?php echo formatnumber($k->jumlah_target_cashin); ?></td>
+        <td align="right"><?php echo formatnumber($k->jml_cashin); ?></td>
+        <td align="right"><?php echo formatnumber($hasilcashin); ?></td>
       </tr>
     <?php $no++;
     } ?>
